15def75e777074d964764f71c5f77f171ada57b9
Will Eagan <Will.Eagan@WILLE-PC>
2018-10-17 15:20:14 -0700

    Added ability to change file path.

--- GetRelesedDocs/MainForm.Designer.vb
+++ GetRelesedDocs/MainForm.Designer.vb
@@ -28,45 +28,51 @@ Partial Class MainForm
         Me.Label1 = New System.Windows.Forms.Label()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Txt_FilePath = New System.Windows.Forms.TextBox()
         Me.StatusStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Btn_Go
         '
         Me.Btn_Go.BackColor = System.Drawing.Color.Lime
+        Me.Btn_Go.Enabled = False
         Me.Btn_Go.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Btn_Go.Location = New System.Drawing.Point(337, 123)
+        Me.Btn_Go.Location = New System.Drawing.Point(277, 214)
         Me.Btn_Go.Margin = New System.Windows.Forms.Padding(4)
         Me.Btn_Go.Name = "Btn_Go"
-        Me.Btn_Go.Size = New System.Drawing.Size(134, 58)
+        Me.Btn_Go.Size = New System.Drawing.Size(187, 60)
         Me.Btn_Go.TabIndex = 0
         Me.Btn_Go.Text = "GO!"
         Me.Btn_Go.UseVisualStyleBackColor = False
         '
         'SearchValues
         '
-        Me.SearchValues.Location = New System.Drawing.Point(13, 93)
+        Me.SearchValues.Enabled = False
+        Me.SearchValues.Location = New System.Drawing.Point(19, 175)
         Me.SearchValues.Margin = New System.Windows.Forms.Padding(4)
         Me.SearchValues.Name = "SearchValues"
-        Me.SearchValues.Size = New System.Drawing.Size(458, 22)
+        Me.SearchValues.Size = New System.Drawing.Size(445, 22)
         Me.SearchValues.TabIndex = 1
         Me.SearchValues.Text = "Enter values here..."
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 23)
+        Me.Label1.Location = New System.Drawing.Point(16, 22)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(327, 48)
+        Me.Label1.Size = New System.Drawing.Size(454, 64)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "1. Enter desired part numbers separated by a comma." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "2. Hit 'GO' button." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "3. Chec" &
-    "k desktop for files."
+        Me.Label1.Text = "1. Check that the file path is pointed to where you would like to search." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "2. Ent" &
+    "er desired numbers separated by a comma (e.g. 54463, 32432, 85711)." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "3. Hit 'GO'" &
+    " button." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "4. Check desktop for files."
         '
         'StatusStrip1
         '
         Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 189)
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 289)
         Me.StatusStrip1.Name = "StatusStrip1"
         Me.StatusStrip1.Padding = New System.Windows.Forms.Padding(1, 0, 19, 0)
         Me.StatusStrip1.Size = New System.Drawing.Size(484, 22)
@@ -79,11 +85,29 @@ Partial Class MainForm
         Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(39, 17)
         Me.ToolStripStatusLabel1.Text = "Status"
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Txt_FilePath)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 105)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(460, 50)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "File Path"
+        '
+        'Txt_FilePath
+        '
+        Me.Txt_FilePath.Location = New System.Drawing.Point(7, 22)
+        Me.Txt_FilePath.Name = "Txt_FilePath"
+        Me.Txt_FilePath.Size = New System.Drawing.Size(445, 22)
+        Me.Txt_FilePath.TabIndex = 0
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(484, 211)
+        Me.ClientSize = New System.Drawing.Size(484, 311)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.SearchValues)
@@ -92,12 +116,14 @@ Partial Class MainForm
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(500, 250)
-        Me.MinimumSize = New System.Drawing.Size(500, 250)
+        Me.MaximumSize = New System.Drawing.Size(500, 350)
+        Me.MinimumSize = New System.Drawing.Size(500, 350)
         Me.Name = "MainForm"
-        Me.Text = "Get Released Docs"
+        Me.Text = "Get Docs"
         Me.StatusStrip1.ResumeLayout(False)
         Me.StatusStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -108,4 +134,6 @@ Partial Class MainForm
     Friend WithEvents Label1 As Label
     Friend WithEvents StatusStrip1 As StatusStrip
     Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Txt_FilePath As TextBox
 End Class
--- GetRelesedDocs/MainForm.vb
+++ GetRelesedDocs/MainForm.vb
@@ -1,6 +1,6 @@
 ﻿Public Class MainForm
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Txt_FilePath.Text = "\\whidbey\data_server\Released Documents"
     End Sub
 
     Private Sub Btn_Go_Click(sender As Object, e As EventArgs) Handles Btn_Go.Click
@@ -27,72 +27,91 @@
     End Sub
 
     Sub getFilesAtDir(nameList() As String)
-
-        Dim releasedDocsPath As String = "\\whidbey\data_server\Released Documents"
-
-        If System.IO.Directory.Exists(releasedDocsPath) Then
-
-            Dim filesToCopy() As String
-            ReDim filesToCopy(0)
-
-            ToolStripStatusLabel1.Text = "Searching for your part numbers"
-            StatusStrip1.Update()
-
-            For Each filename As String In IO.Directory.GetFiles(releasedDocsPath, "*", IO.SearchOption.AllDirectories)
-                For i = 0 To UBound(nameList)
-                    If nameList(i) <> "" And Len(nameList(i)) = 5 Then
-                        If InStr(IO.Path.GetFileName(filename), nameList(i)) Then
-                            If filesToCopy(0) <> "" Then
-                                ReDim Preserve filesToCopy(UBound(filesToCopy) + 1)
-                            End If
-                            filesToCopy(UBound(filesToCopy)) = filename
-                        End If
-                    End If
-                Next
-            Next
-
-
-
-            Dim errMsg As String = ""
-
-            For i = 0 To UBound(nameList)
-                Dim foundEntity As Boolean = 0
-                If nameList(i) <> "" And Len(nameList(i)) = 5 Then
-                    For z = 0 To UBound(filesToCopy)
-                        If InStr(IO.Path.GetFileName(filesToCopy(z)), nameList(i)) > 0 Then
-                            foundEntity = 1
-                        End If
-                    Next
-                End If
-
-                If foundEntity = 0 Then
-                    errMsg = errMsg & nameList(i) & vbNewLine
-                End If
-            Next
-
-
-            Dim desktopPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
-            desktopPath = desktopPath & "\" & "outputFiles_" & Now.Hour.ToString & Now.Minute.ToString & Now.Second.ToString
-
-            IO.Directory.CreateDirectory(desktopPath)
-
-            If filesToCopy(0) <> "" Then
-                For i = 0 To UBound(filesToCopy)
-                    ToolStripStatusLabel1.Text = "Copying: " & i + 1 & " Of " & UBound(filesToCopy) + 1
-                    StatusStrip1.Update()
-
-                    My.Computer.FileSystem.CopyFile(filesToCopy(i), desktopPath & "\" & System.IO.Path.GetFileName(filesToCopy(i)), Microsoft.VisualBasic.FileIO.UIOption.AllDialogs, Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing)
-                Next
-            End If
-
-            If errMsg <> "" Then
-                MsgBox("Files have been copied to your desktop." & vbNewLine & "The following files were not located:" & vbNewLine & errMsg,, "Error")
-            Else
-                MsgBox("Files have been copied to your desktop.",, "Complete")
-            End If
-        Else
-            Err.Raise(1,, "Cannot locate released documents on server and path '" & releasedDocsPath)
+        If Not IO.Directory.Exists(Txt_FilePath.Text) Then
+            Call dirCheck()
+            MsgBox("Directory does not exist",, "Error")
+            Exit Sub
         End If
 
+        Dim releasedDocsPath As String = Txt_FilePath.Text
+
+        Dim filesToCopy() As String
+        ReDim filesToCopy(0)
+
+        ToolStripStatusLabel1.Text = "Searching for your numbers"
+        StatusStrip1.Update()
+
+        For Each filename As String In IO.Directory.GetFiles(releasedDocsPath, "*", IO.SearchOption.AllDirectories)
+            For i = 0 To UBound(nameList)
+                If nameList(i) <> "" And Len(nameList(i)) = 5 Then
+                    If InStr(IO.Path.GetFileName(filename), nameList(i)) Then
+                        If filesToCopy(0) <> "" Then
+                            ReDim Preserve filesToCopy(UBound(filesToCopy) + 1)
+                        End If
+                        filesToCopy(UBound(filesToCopy)) = filename
+                    End If
+                End If
+            Next
+        Next
+
+
+
+        Dim errMsg As String = ""
+
+        For i = 0 To UBound(nameList)
+            Dim foundEntity As Boolean = 0
+            If nameList(i) <> "" And Len(nameList(i)) = 5 Then
+                For z = 0 To UBound(filesToCopy)
+                    If InStr(IO.Path.GetFileName(filesToCopy(z)), nameList(i)) > 0 Then
+                        foundEntity = 1
+                    End If
+                Next
+            End If
+
+            If foundEntity = 0 Then
+                errMsg = errMsg & nameList(i) & vbNewLine
+            End If
+        Next
+
+
+        Dim desktopPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+        desktopPath = desktopPath & "\" & "outputFiles_" & Now.Hour.ToString & Now.Minute.ToString & Now.Second.ToString
+
+        IO.Directory.CreateDirectory(desktopPath)
+
+        If filesToCopy(0) <> "" Then
+            For i = 0 To UBound(filesToCopy)
+                ToolStripStatusLabel1.Text = "Copying: " & i + 1 & " Of " & UBound(filesToCopy) + 1
+                StatusStrip1.Update()
+
+                My.Computer.FileSystem.CopyFile(filesToCopy(i), desktopPath & "\" & System.IO.Path.GetFileName(filesToCopy(i)), Microsoft.VisualBasic.FileIO.UIOption.AllDialogs, Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing)
+            Next
+        End If
+
+        If errMsg <> "" Then
+            MsgBox("Files have been copied to your desktop." & vbNewLine & "The following files were not located:" & vbNewLine & errMsg,, "Error")
+        Else
+            MsgBox("Files have been copied to your desktop.",, "Complete")
+        End If
+
+
     End Sub
+
+    Private Sub Txt_FilePath_TextChanged(sender As Object, e As EventArgs) Handles Txt_FilePath.TextChanged
+        Call dirCheck()
+    End Sub
+
+    Private Sub dirCheck()
+        If IO.Directory.Exists(Txt_FilePath.Text) Then
+            Txt_FilePath.BackColor = Color.White
+            SearchValues.Enabled = True
+            Btn_Go.Enabled = True
+        Else
+            Txt_FilePath.BackColor = Color.Red
+            SearchValues.Enabled = False
+            Btn_Go.Enabled = False
+        End If
+    End Sub
+
+
 End Class
